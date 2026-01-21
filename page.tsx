f4a0847e9311ddc4b496be8a856f2b045b650ba3
NUOXIAOYAO0521 <bishengnuonuo@gmail.com>
2026-01-21 11:27:16 +0900

    删除 page.tsx
    Remove root page.tsx (App Router fix)

--- page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Yilin Corporation</h1>
-      <p>Premium Mobility Solutions Worldwide</p>
-      <p>From $XX / day</p>
-    </main>
-  );
-}
